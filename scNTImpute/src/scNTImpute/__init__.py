0c35cfa0f91e72774fa78be517b73e8d3ee224cc
qiyueyang-7 <127671464+qiyueyang-7@users.noreply.github.com>
2023-05-18 16:45:22 +0800

    Delete scNTImpute directory

--- scNTImpute/src/scNTImpute/__init__.py
+++
@@ -1,6 +0,0 @@
-from scNTImpute.logging_utils import initialize_logger
-from scNTImpute.models import scNTImpute, scVI
-from scNTImpute.trainers import UnsupervisedTrainer, MMDTrainer, BatchAdversarialTrainer, prepare_for_transfer, train_test_split, set_seed
-from scNTImpute.eval_utils import evaluate, calculate_entropy_batch_mixing, calculate_kbet, clustering, draw_embeddings, set_figure_params
-
-initialize_logger()
--- scNTImpute/src/scNTImpute/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scNTImpute import scNTImpute
-from .scVI import scVI
-from .BaseCellModel import BaseCellModel
-from .model_utils import get_fully_connected_layers, get_kl
